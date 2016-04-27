465647704d887e8bb45c2c67f989c083462a67f7
canzhiye <canzhiye@gmail.com>
2016-04-27 12:33:12 -0700

    remove csv in wrong place

--- scraper.py
+++ scraper.py
@@ -1,2 +1,7 @@
 import requests
 
+#http://stats.nba.com/stats/draftcombineplayeranthro?LeagueID=00&SeasonYear=2015-16
+#http://stats.nba.com/stats/draftcombinedrillresults?LeagueID=00&SeasonYear=2015-16	200	GET	stats.nba.com	/stats/draftcombinedrillresults?LeagueID=00&SeasonYear=2015-16
+
+r = requests.get('http://stats.nba.com/stats/draftcombineplayeranthro?LeagueID=00&SeasonYear=2015-16')
+print(r.json()['resultSets'][0]['headers'])
